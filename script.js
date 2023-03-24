a1e99f50be25a1f8effaad1521948f8eafe55e8b
gabiprosfriends <127973347+gabiprosfriends@users.noreply.github.com>
2023-03-24 11:42:00 +0000

    code incomplet

--- script.js
+++ script.js
@@ -1,10 +1,81 @@
-const btn = document.getElementsByClassName('cell');
-const arrays = document.getElementsByClassName('board')
+const btns = document.getElementsByClassName('cell');
+let turn = true;
+let contadorx = 0;
+let contadoro = 0;
 
-for(let i = 0; i <= arrays.length; i++)
-{
-    console.log(arrays[i]);
-    arrays[i].addEventListener("click", () => {
-        arrays[i].innerHTML = "x";
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", () => {
+        btns[i].disabled = true;
+        if (turn) {
+            btns[i].innerHTML = "x";
+        }
+        else {
+            btns[i].innerHTML = "o";
+        }
+        if (turn) {
+            turn = false;
+        }
+        else {
+            turn = true;
+        }
+        console.log(turn)
+
+        if (btns[0 == 1] && btns[1 == 2]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else if (btns[0 == 3] && btns[3 == 6]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else if (btns[6 == 7] && btns[7 == 8]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else if (btns[8 == 4] && btns[4 == 2]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else if (btns[2 == 4] && btns[4 == 6]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else if (btns[0 == 4] && btns[4 == 8]) {
+            if (turn) {
+                contadorx++;
+            }
+            else {
+                contadoro++;
+            }
+        }
+
+        else {
+        }
     })
 }
